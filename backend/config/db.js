f95e3a104ac9f95db4f1469875ab52b9b0d4d33a
Knights78 <saifushaikh102@gmail.com>
2024-06-24 12:19:02 +0530

    test

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
   const connectDB= async ()=>{
-    
+    await mongoose.connect('mongodb+srv://saifushaikh102:84510@cluster0.a3ooqhc.mongodb.net/food-del').then(()=>console.log("database connected"))
 }
 
 export default connectDB;
